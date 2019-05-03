169babaafa9a059e68f0ba290d0c20d90b8f8e43
Natalie J <natsetc@gmail.com>
2019-05-03 20:36:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,8 @@ if phrase == phrase.upcase
   puts "NO, NOT SINCE 1938!"
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
-elsif phrase == 
+elsif phrase == "I LOVE YOU GRANDMA!"
+  puts 
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
